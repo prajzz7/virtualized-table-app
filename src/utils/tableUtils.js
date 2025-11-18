703a92f93323cc83cf2bf3f9054d4a1877ab2f71
Prajwal Pandeshwar <prajwal.pandeshwar@ansell.com>
2025-11-19 04:34:39 +0530

    style: table height reduced

--- src/utils/tableUtils.js
+++ src/utils/tableUtils.js
@@ -3,4 +3,4 @@ export const TOTAL_ROWS = 10000;
 export const INITIAL_LOAD_COUNT = 500;
 export const CHUNK_SIZE = 500;
 export const RowHeight = 60; // Fixed row height for custom virtualization
-export const LIST_HEIGHT = 600; // Fixed height for the container
+export const LIST_HEIGHT = 580; // Fixed height for the container
